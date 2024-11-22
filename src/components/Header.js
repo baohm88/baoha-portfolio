d0bfe63c5caf416b31cc27f2c2a58ffef5bc2322
Ha Manh Bao <131301257+baohm88@users.noreply.github.com>
2024-11-22 14:24:54 +0700

    add new resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,7 @@ const Header = () => {
                     </a>
                     <button className="btn btn-sm">
                         <a
-                            href="https://drive.google.com/file/d/13O0mWDLPA-kTBvFoC6fKZpeYyszqbmUF/view?usp=sharing"
+                            href="https://drive.google.com/file/d/1ppJ1hTrNlveryTk_AutbinhiC6FfXcjL/view?usp=sharing"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
